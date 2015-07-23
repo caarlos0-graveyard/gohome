3c5c51356907080b06f1cbf6430a814f249f052c
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-07-23 20:45:13 -0300

    using os/user instead of Getenv(HOME)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package gohome
 
 import (
 	"os"
+	"os/user"
 	"path/filepath"
 	"runtime"
 )
@@ -61,5 +62,9 @@ func appData(appName string) string {
 }
 
 func home() string {
-	return os.Getenv("HOME")
+	u, err := user.Current()
+	if err != nil {
+		panic(err.Error())
+	}
+	return u.HomeDir
 }
