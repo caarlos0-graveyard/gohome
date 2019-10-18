35f18c3c52bd5fe8fb9afafbbdfab742f8283276
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-10-18 09:41:31 -0300

    feat: deprecated
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- main_darwin_test.go
+++ main_darwin_test.go
@@ -2,6 +2,7 @@ package gohome
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,11 +15,11 @@ var home = os.Getenv("HOME")
 func TestCache(t *testing.T) {
 	cache := Cache(appName)
 	assert.NotEmpty(t, cache)
-	assert.Equal(t, home+"/Library/Caches/"+appName, cache)
+	assert.Equal(t, filepath.Join(home, "/Library/Caches", appName), cache)
 }
 
 func TestConfig(t *testing.T) {
 	config := Config(appName)
 	assert.NotEmpty(t, config)
-	assert.Equal(t, home+"/Library/Application Support/"+appName, config)
+	assert.Equal(t, filepath.Join(home, "/Library/Application Support/", appName), config)
 }
